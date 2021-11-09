46935eddd326055290924c7a25153543d83f7b2a
PEI-YUN LEE <may1092200258@gmail.com>
2021-11-10 02:04:04 +0800

    module test

--- linebot_api/api.py
+++ linebot_api/api.py
@@ -1,5 +1,3 @@
-
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,4 +5,3 @@ from linebot import (
 line_bot_api = LineBotApi(
     'qDvsOUDQMWXwONG+aH+fwhbeD5qU7Cs9okmn5FngnuXAZlykbPx77W6O5cuvPAjVloywUpG3ZD1hSMTT28SvctIcybUYHJlAmJY2e4KzbeHeUDVVmfAbeZwqZiDPAafYoyUSdpd59O0ARk937Vv7VwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('dcc7f9bee687f90fe33cc309962f1210')
-

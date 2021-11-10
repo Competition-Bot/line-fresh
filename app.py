d65a2c9a1a74c52b94ad56210fe7538ba66e4829
PEI-YUN LEE <may1092200258@gmail.com>
2021-11-10 20:51:02 +0800

    test

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackAction, MessageAction, URIAction, ImageMessage, messages
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
 )
 
 app = Flask(__name__)
@@ -48,7 +48,7 @@ def handle_message(event):
             event.reply_token,
             [TextSendMessage(text="以前阿嬤常常帶我去虎尾糖廠買冰棒吃～然後坐在月台看火車～小時候我曾經在作文上寫我要當火車司機呢！"),
              TextSendMessage(text="我最喜歡這班車！"),
-             ImageMessage(originalContentUrl="https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg",
+             ImageSendMessage(originalContentUrl="https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg",
                           previewImageUrl="https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg"),
              TextSendMessage(text="請輸入『我搭00：00的000出發！』(00：00為時間、000為車種)"),
              TextSendMessage(text="我搭14：00的2077出發！』(00：00為時間、000為車種)")])

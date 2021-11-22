2a5efab3d0c1dea15bcd8daec2af0e88649116f9
may <may1092200258@gmail.com>
2021-11-22 22:30:09 +0800

    fix three

--- level/three.py
+++ level/three.py
@@ -12,7 +12,7 @@ def levelthree_message(event):
             event.reply_token,
             [
                 TextSendMessage(
-                    text="「日記2」 %0D%0A 這裡是吳哥哥的家，還記得以前他總是告訴我們許多好玩的醫學故事，我們聽的超級開心的!前幾年他考上大學就在北部念書了，不知道他過得好不好，不過我相信他一定是個好醫生的! %0D%0A  %0D%0A 還記得以前中午一到我們就會守在電視機前面，看史艷文如何化解難關，以下的暗號，我想你一定看得懂吧，我們老地方見~"),
+                    text="「日記2」\n這裡是吳哥哥的家，還記得以前他總是告訴我們許多好玩的醫學故事，我們聽的超級開心的!前幾年他考上大學就在北部念書了，不知道他過得好不好，不過我相信他一定是個好醫生的!\n\n還記得以前中午一到我們就會守在電視機前面，看史艷文如何化解難關，以下的暗號，我想你一定看得懂吧，我們老地方見~"),
                 ImageSendMessage("https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg",
                                  "https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg"),
                 TextSendMessage(

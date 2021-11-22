b51fc576e45c7b4d1c4e3b73787af08fa1ea1285
may <may1092200258@gmail.com>
2021-11-22 22:11:31 +0800

    test

--- app.py
+++ app.py
@@ -52,25 +52,7 @@ level = 0
 def handle_message(event):
     global level
     if(event.message.text == 'start'):
-        line_bot_api.reply_message(
-            event.reply_token,
-            TemplateSendMessage(
-                alt_text='虎尾手札 - 穿梭巷弄的少女',
-                template=ButtonsTemplate(
-                    thumbnail_image_url='https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg',
-                    imageAspectRatio='rectangle',
-                    imageSize='cover',
-                    imageBackgroundColor='#FFFFFF',
-                    title='虎尾手札 - 穿梭巷弄的少女',
-                    text='小春與小欣是從小一起在虎尾長大的好朋友',
-                    actions=[
-                        MessageAction(
-                         label='開始遊戲',
-                         text='開始遊戲'
-                        ),
-                    ]
-                )
-            ))
+        start_message(event)
     elif(event.message.text == '開始遊戲' and level == 0):
         levelzero_message(event)
     elif(event.message.text == '打開日記' and level == 0):

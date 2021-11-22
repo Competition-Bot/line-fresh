803127b02d3c1e426bfd838ca6b4602fb9f2434a
may <may1092200258@gmail.com>
2021-11-22 22:46:14 +0800

    fix

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def handle_message(event):
     elif((event.message.text == '開始遊戲' and level == 'start') or event.message.text == 'test0'):
         levelzero_message(event)
         level = 0
-    elif((event.message.text == '開啟日記' and level == 0) or event.message.text == 'test1'):
+    elif((event.message.text == '打開日記' and level == 0) or event.message.text == 'test1'):
         level = 1
         levelone_message(event)
     elif((event.message.text == '2B' and level == 1) or event.message.text == 'test2'):

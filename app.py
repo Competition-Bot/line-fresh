aae5e3469bd60f5948ae0d030addd80fe4d7312e
may <may1092200258@gmail.com>
2021-11-22 20:42:24 +0800

    test

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def callback():
 
 level = 1
 
+line_bot_api.push_message('<to>', TextSendMessage(text='Hello World!'))
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):

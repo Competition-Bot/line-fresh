102c51e1a05c88215cb97b2ce11822d2c823c4df
may <may1092200258@gmail.com>
2021-11-23 17:45:54 +0800

    test

--- level/five.py
+++ level/five.py
@@ -77,7 +77,30 @@ def levelfive_message(event):
                         )
                     )
                 ),
-                TextSendMessage(
-                    text="(輸入『前往0000』，0000是地點名稱，00數量僅供參考)\n\n(線索在合同廳舍的門牌號碼中，輸入正確的收音機頻道)")
+                FlexSendMessage(
+                    alt_text='start',
+                    contents=BubbleContainer(
+                        size="kilo",
+                        direction="ltr",
+                        body={
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "輸入『前往0000』,0000是地點名稱,00數量僅供參考目吧」",
+                                    "color": "#666666",
+                                    "wrap": True,
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "線索在合同廳舍的門牌號碼中,透過liff輸入正確的收音機頻道",
+                                    "color": "#666666",
+                                    "wrap": True,
+                                    "margin": "md"
+                                }
+                            ]
+                        }
+                    )),
             ]
         ))

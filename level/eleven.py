d3531728dde5db731a5817ab83a5fce9305f6e83
may <may1092200258@gmail.com>
2021-11-23 18:40:08 +0800

    test

--- level/eleven.py
+++ level/eleven.py
@@ -47,55 +47,6 @@ def leveleleven_message(event):
                                             "size": "full",
                                             "aspectMode": "cover",
                                             "gravity": "center"
-                                        },
-                                        {
-                                            "type": "box",
-                                            "layout": "vertical",
-                                            "contents": [],
-                                            "position": "absolute",
-                                            "background": {
-                                                "type": "linearGradient",
-                                                "angle": "0deg",
-                                                "endColor": "#00000000",
-                                                "startColor": "#00000099"
-                                            },
-                                            "width": "100%",
-                                            "height": "40%",
-                                            "offsetBottom": "0px",
-                                            "offsetStart": "0px",
-                                            "offsetEnd": "0px"
-                                        },
-                                        {
-                                            "type": "box",
-                                            "layout": "horizontal",
-                                            "contents": [
-                                                {
-                                                    "type": "box",
-                                                    "layout": "vertical",
-                                                    "contents": [
-                                                        {
-                                                            "type": "box",
-                                                            "layout": "horizontal",
-                                                            "contents": [
-                                                                {
-                                                                    "type": "text",
-                                                                    "text": "A 新茜娜面霜",
-                                                                    "size": "md",
-                                                                    "color": "#ffffff",
-                                                                    "align": "center",
-                                                                    "weight": "bold"
-                                                                }
-                                                            ]
-                                                        }
-                                                    ],
-                                                    "spacing": "xs"
-                                                }
-                                            ],
-                                            "position": "absolute",
-                                            "offsetBottom": "0px",
-                                            "offsetStart": "0px",
-                                            "offsetEnd": "0px",
-                                            "paddingAll": "20px"
                                         }
                                     ],
                                     "paddingAll": "0px"

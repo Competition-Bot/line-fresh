bb0fcd9d999236f0d0496051d0b089437869486d
may <may1092200258@gmail.com>
2021-11-23 20:12:05 +0800

    test

--- level/eleven.py
+++ level/eleven.py
@@ -12,5 +12,7 @@ def leveleleven_message(event):
             [
                 TextSendMessage(
                     text="原來虎珍的前身是正義百貨行，真是一棟有歷史的建築！"),
+                TextSendMessage(
+                    text="「日記7」\n\n這裡好多漂亮的裙子喔！幸好我們跟老闆娘阿姨很熟，可以來這裡玩！我們最喜歡在這裡想像我們結婚的樣子了！說好了我們要當彼此的伴娘喔！")
             ]
         ))

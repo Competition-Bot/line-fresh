43058538eae91c273188f71701062fa5c7c71e52
may <may1092200258@gmail.com>
2021-11-23 15:08:14 +0800

    start

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    JoinEvent, MessageEvent, TextMessage, FlexSendMessage, BubbleContainer, ImageComponent, TextComponent, BoxComponent, TextSendMessage, BoxComponent, ImageSendMessage, MessageAction, URIAction, ButtonsTemplate, PostbackAction, TemplateSendMessage
+    JoinEvent, MessageEvent, TextMessage,  TextSendMessage, BoxComponent, ImageSendMessage, MessageAction, URIAction, ButtonsTemplate, PostbackAction, TemplateSendMessage
 )
 
 from api.lineBotApi import line_bot_api
@@ -71,7 +71,7 @@ def handle_message(event):
     elif((event.message.text == '打開日記' and level == '0') or event.message.text == 'test1'):
         level = '1'
         levelone_message(event)
-    elif((event.message.text == '2B' and level == '1') or event.message.text == 'test2'):
+    elif((event.message.text == '4B' and level == '1') or event.message.text == 'test2'):
         level = '2'
         leveltwo_message(event)
     elif(((event.message.text == '有6隻石頭鳥' or event.message.text == '有六隻石頭鳥') and level == '2') or event.message.text == 'test3'):
@@ -116,81 +116,6 @@ def handle_message(event):
     elif((event.message.text == '五分車' and level == '13-3') or event.message.text == 'test10'):
         level = 'end'
         levelend_message(event)
-    elif(event.message.text == 'test'):
-        line_bot_api.reply_message(
-            event.reply_token,
-            line_bot_api.reply_message(
-                event.reply_token,
-                FlexSendMessage(
-                    alt_text='test',
-                    contents=BubbleContainer(
-                        hero=ImageComponent(
-                            url="https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg",
-                            size="full",
-                            aspectRatio="20:13",
-                            aspectMode="cover",
-                        ),
-                        body=BoxComponent(
-                            layout="vertical",
-                            contents=[
-                                BoxComponent(
-                                    layout="vertical",
-                                    margin="lg",
-                                    spacing="sm",
-                                    contents=[
-                                        {
-                                            "type": "text",
-                                            "text": "虎尾手札 - 穿梭巷弄的少女",
-                                            "weight": "bold",
-                                            "size": "lg"
-                                        },
-                                        {
-                                            "type": "box",
-                                            "layout": "vertical",
-                                            "margin": "md",
-                                            "spacing": "sm",
-                                            "contents": [
-                                                {
-                                                    "type": "box",
-                                                    "layout": "baseline",
-                                                    "spacing": "sm",
-                                                    "contents": [
-                                                        {
-                                                            "type": "text",
-                                                            "text": "小春與小花是要好的朋友，她們喜歡在巷弄間到處探險，藏著她倆的秘密，升上高中後小春將搬離虎尾去外地，因此她將策劃一場紀念她們友誼的冒險...",
-                                                            "wrap": True,
-                                                            "color": "#666666",
-                                                            "size": "sm",
-                                                            "flex": 5
-                                                        }
-                                                    ]
-                                                },
-                                                {
-                                                    "type": "box",
-                                                    "layout": "baseline",
-                                                    "spacing": "sm",
-                                                    "contents": [
-                                                        {
-                                                            "type": "text",
-                                                            "text": "玩家將扮演撿到日記本的角色，將依循日記的指示探索虎尾的巷弄，進而發現小春與小花之間的點點滴滴。",
-                                                            "wrap": True,
-                                                            "color": "#666666",
-                                                            "size": "sm",
-                                                            "flex": 5
-                                                        }
-                                                    ],
-                                                    "margin": "md"
-                                                }
-                                            ]
-                                        }
-                                    ]
-                                )
-                            ]
-                        )
-                    )
-                )
-            )
-        )
 
 
 @handler.add(JoinEvent, message=TextMessage)
--- level/start.py
+++ level/start.py
@@ -1,5 +1,5 @@
 from linebot.models import (
-    MessageAction, ButtonsTemplate, TemplateSendMessage
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
 )
 
 from api.lineBotApi import line_bot_api
@@ -8,20 +8,83 @@ from api.lineBotApi import line_bot_api
 def start_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TemplateSendMessage(
-            alt_text='虎尾手札 - 穿梭巷弄的少女',
-            template=ButtonsTemplate(
-                thumbnail_image_url='https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg',
-                imageAspectRatio='rectangle',
-                imageSize='cover',
-                imageBackgroundColor='#FFFFFF',
-                title='虎尾手札 - 穿梭巷弄的少女',
-                text='小春與小欣是從小一起在虎尾長大的好朋友',
-                actions=[
-                    MessageAction(
-                        label='開始遊戲',
-                        text='開始遊戲'
+        line_bot_api.reply_message(
+            event.reply_token,
+            FlexSendMessage(
+                alt_text='start',
+                contents=BubbleContainer(
+                    hero=ImageComponent(
+                        url="https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg",
+                        size="full",
+                        aspectRatio="20:13",
+                        aspectMode="cover",
                     ),
-                ]
+                    body=BoxComponent(
+                        layout="vertical",
+                        contents=[
+                            BoxComponent(
+                                layout="vertical",
+                                margin="lg",
+                                spacing="sm",
+                                contents=[
+                                    {
+                                        "type": "text",
+                                        "text": "虎尾手札 - 穿梭巷弄的少女",
+                                        "weight": "bold",
+                                        "size": "lg"
+                                    },
+                                    {
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "margin": "md",
+                                        "spacing": "sm",
+                                        "contents": [
+                                            {
+                                                "type": "box",
+                                                "layout": "baseline",
+                                                "spacing": "sm",
+                                                "contents": [
+                                                    {
+                                                        "type": "text",
+                                                        "text": "小春與小花是要好的朋友，她們喜歡在巷弄間到處探險，藏著她倆的秘密，升上高中後小春將搬離虎尾去外地，因此她將策劃一場紀念她們友誼的冒險...",
+                                                        "wrap": True,
+                                                        "color": "#666666",
+                                                        "size": "sm",
+                                                        "flex": 5
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "type": "box",
+                                                "layout": "baseline",
+                                                "spacing": "sm",
+                                                "contents": [
+                                                    {
+                                                        "type": "text",
+                                                        "text": "玩家將扮演撿到日記本的角色，將依循日記的指示探索虎尾的巷弄，進而發現小春與小花之間的點點滴滴。",
+                                                        "wrap": True,
+                                                        "color": "#666666",
+                                                        "size": "sm",
+                                                        "flex": 5
+                                                    }
+                                                ],
+                                                "margin": "md"
+                                            },
+                                            {
+                                                "type": "button",
+                                                "action": {
+                                                    "type": "message",
+                                                    "label": "開始遊戲",
+                                                    "text": "開始遊戲"
+                                                }
+                                            }
+                                        ]
+                                    }
+                                ]
+                            )
+                        ]
+                    )
+                )
             )
-        ))
+        )
+    )

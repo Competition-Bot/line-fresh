a8992b569122da3eee8bf83fedffe002c9a69cf5
may <may1092200258@gmail.com>
2021-11-23 21:38:13 +0800

    test0

--- level/zero.py
+++ level/zero.py
@@ -1,5 +1,5 @@
 from linebot.models import (
-    TextSendMessage, TemplateSendMessage, ButtonsTemplate, MessageAction
+    TextSendMessage, TemplateSendMessage, ButtonsTemplate, MessageAction, FlexSendMessage
 )
 
 from api.lineBotApi import line_bot_api
@@ -13,21 +13,53 @@ def levelzero_message(event):
             [
                 TextSendMessage(
                     text="我是一個到處旅行的旅人，這天，我來到了虎尾旅行，走著走著我在地上撿到了一本日記本"),
-                TemplateSendMessage(
+                FlexSendMessage(
                     alt_text='陳舊的日記本',
-                    template=ButtonsTemplate(
-                        thumbnail_image_url='https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg',
-                        imageAspectRatio='rectangle',
-                        imageSize='cover',
-                        imageBackgroundColor='#FFFFFF',
-                        text='陳舊的日記本',
-                        actions=[
-                            MessageAction(
-                                label='打開日記',
-                                text='打開日記'
-                            )
-                        ]
-                    )
-                )
+                    contents={
+                        "type": "bubble",
+                        "size": "kilo",
+                        "hero": {
+                            "type": "image",
+                            "url": "https://ithelp.ithome.com.tw/upload/images/20200111/201068658m7crqYkfm.jpg",
+                            "size": "full",
+                            "aspectMode": "cover",
+                            "aspectRatio": "20:14"
+                        },
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "陳舊的日記本",
+                                    "wrap": True,
+                                    "size": "md",
+                                    "weight": "bold"
+                                }
+                            ],
+                            "offsetTop": "none",
+                            "paddingAll": "lg"
+                        },
+                        "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "button",
+                                    "action": {
+                                        "type": "message",
+                                        "label": "打開日記",
+                                        "text": '打開日記'
+                                    },
+                                    "height": "sm"
+                                }
+                            ]
+                        },
+                        "styles": {
+                            "body": {
+                                "separator": True
+                            }
+                        }
+                    })
             ]
         ))

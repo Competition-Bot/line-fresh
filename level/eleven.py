ecb9c2f9b4b9f788fe219e6148c836ff83509b6d
may <may1092200258@gmail.com>
2021-11-23 20:09:50 +0800

    test

--- level/eleven.py
+++ level/eleven.py
@@ -10,22 +10,7 @@ def leveleleven_message(event):
         event.reply_token,
         line_bot_api.reply_message(
             [
-                TemplateSendMessage(
-                    alt_text='虎珍堂',
-                    template=ButtonsTemplate(
-                        thumbnail_image_url='https://joes.tw/wp-content/uploads/20190719193350_27.jpg',
-                        imageAspectRatio='rectangle',
-                        imageSize='cover',
-                        imageBackgroundColor='#FFFFFF',
-                        title='虎珍堂',
-                        text='虎珍堂菓寮店～70年日治時代老屋風華再現，用地瓜與乳酪蛋糕做成散步美食「虎月燒」！',
-                        actions=[
-                            URIAction(
-                                label='查看熱點資訊',
-                                uri='https://liff.line.me/1582347558-VdW5GZDw/detail/486254094835523175?utm_campaign=486254094835523175&utm_medium=CopyURL&utm_source=Share'
-                            )
-                        ]
-                    )
-                ),
+                TextSendMessage(
+                    text="原來虎珍的前身是正義百貨行，真是一棟有歷史的建築！"),
             ]
         ))

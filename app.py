988a91249386aeaada02335c3e79bc1a23686bbb
may <may1092200258@gmail.com>
2021-11-24 16:10:01 +0800

    test

--- app.py
+++ app.py
@@ -64,6 +64,7 @@ def handle_message(event):
     global level
     if((event.message.text == 'start' and level == 'init') or event.message.text == 'start'):
         start_message(event)
+        start_message(event)
         level = 'start'
     elif((event.message.text == '開始遊戲' and level == 'start') or event.message.text == 'test0'):
         levelzero_message(event)
--- level/zero.py
+++ level/zero.py
@@ -63,8 +63,3 @@ def levelzero_message(event):
                 })
         ]
     )
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(
-            text="我是一個到處旅行的旅人，這天，我來到了虎尾旅行，走著走著我在地上撿到了一本日記本"),
-    )

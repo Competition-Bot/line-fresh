e24d420a015a4901b66d57c939cd9c777766db09
Ruby Yeh <hyoka4654@gmail.com>
2021-11-29 17:47:57 +0800

    fix-elif順序

--- app.py
+++ app.py
@@ -73,8 +73,73 @@ def handle_message(event):
         level = '0'
     elif((event.message.text == '打開日記' and level == '0') or event.message.text == 'test1'):
         level = '1'
-        help == '1'
+        help = '1'
         levelone_message(event)
+    
+
+
+    
+    elif((event.message.text == '虎尾厝沙龍' and level == '1') or event.message.text == 'test2'):
+        level = '2'
+        help = '1'
+        leveltwo_message(event)
+
+    elif(((event.message.text == '有6隻石頭鳥' or event.message.text == '有六隻石頭鳥') and level == '2') or event.message.text == 'test3'):
+        help = '1'
+        level = '3'
+        levelthree_message(event)
+    elif((event.message.text == '前往合同廳舍' and level == '3') or event.message.text == 'test4'):
+        level = '4'
+        help = '1'
+        levelfour_message(event)   
+    elif(((event.message.text == '少了7隻' or event.message.text == '少了七隻') and level == '4') or event.message.text == 'test5'):
+        level = '5'
+        help = '1'
+        levelfive_message(event)
+    elif((event.message.text == '前往雲林布袋戲館' and level == '5') or event.message.text == 'test6'):
+        level = '6'
+        help = '1'
+        levelsix_message(event)
+    elif((event.message.text == '生、丑' and level == '6') or event.message.text == 'test7'):
+        level = '7'
+        help = '1'
+        levelseven_message(event)
+    elif((event.message.text == '屋敷' and level == '7') or event.message.text == 'test8'):
+        level = '8'
+        help = '1'
+        leveleight_message(event)
+    elif(((event.message.text == 'COOL' or event.message.text == 'cool') and level == '8') or event.message.text == 'test9'):
+        level = '9'
+        help = '1'
+        levelnine_message(event)
+    elif((event.message.text == '1921' and level == '9') or event.message.text == 'test10'):
+        level = '10'
+        help = '1'
+        levelten_message(event)
+    elif((event.message.text == '前往虎珍堂' and level == '10') or event.message.text == 'test11'):
+        level = '11'
+        help = '1'
+        leveleleven_message(user_id)
+    elif(((event.message.text == 'EADCB' or event.message.text == 'eadcb') and level == '11') or event.message.text == 'test12'):
+        level = '12'
+        help = '1'
+        leveltwelve_message(event)
+    elif(((event.message.text == '我搭14:00的區間車出發' or event.message.text == '我搭14：00的區間車出發') and level == '12') or event.message.text == 'test13'):
+        level = '13-1'
+        help = '1'
+        levelthirteen_one_message(event)
+    elif(((event.message.text == '8' or event.message.text == '八') and level == '13-1')):
+        level = '13-2'
+        help = '1'
+        levelthirteen_two_message(event)
+    elif(((event.message.text == '3' or event.message.text != '三') and level == '13-2')):
+        level = '13-3'
+        help = '1'
+        levelthirteen_three_message(event)
+    elif((event.message.text == '五分車' and level == '13-3') or event.message.text == 'end'):
+        level = 'end'
+        levelend_message(event)
+
 
 
     elif((event.message.text == '我需要幫忙' and level == '1')):
@@ -85,10 +150,7 @@ def handle_message(event):
             Help_2(event,level)
     elif((event.message.text != '虎尾厝沙龍' and level == '1')): #嘿嘿答錯
         error_Nohelp(event,help)
-    elif((event.message.text == '虎尾厝沙龍' and level == '1') or event.message.text == 'test2'):
-        level = '2'
-        help == '1'
-        leveltwo_message(event)
+    
 
 
     elif((event.message.text == '我需要幫忙' and level == '2')):
@@ -100,10 +162,7 @@ def handle_message(event):
     elif((event.message.text != '有6隻石頭鳥' and event.message.text != '有六隻石頭鳥' and level == '2')): #嘿嘿答錯
         error_Nohelp(event,help)
     
-    elif(((event.message.text == '有6隻石頭鳥' or event.message.text == '有六隻石頭鳥') and level == '2') or event.message.text == 'test3'):
-        help == '1'
-        level = '3'
-        levelthree_message(event)
+    
 
 
     elif((event.message.text == '我需要幫忙' and level == '3')):
@@ -114,10 +173,7 @@ def handle_message(event):
             Help_2(event,level)
     elif((event.message.text != '前往合同廳舍' and level == '3')): #嘿嘿答錯
         error_Nohelp(event,help)
-    elif((event.message.text == '前往合同廳舍' and level == '3') or event.message.text == 'test4'):
-        level = '4'
-        help == '1'
-        levelfour_message(event)
+    
     
 
     elif((event.message.text == '我需要幫忙' and level == '4')):
@@ -131,10 +187,8 @@ def handle_message(event):
             Help_3(event,level)
     elif(((event.message.text != '少了7隻' and event.message.text != '少了七隻') and level == '4')):
         error_Nohelp(event,help)
-    elif(((event.message.text == '少了7隻' or event.message.text == '少了七隻') and level == '4') or event.message.text == 'test5'):
-        level = '5'
-        help == '1'
-        levelfive_message(event)
+
+
     
 
     elif((event.message.text == '我需要幫忙' and level == '5')):
@@ -148,10 +202,7 @@ def handle_message(event):
             Help_3(event,level)
     elif((event.message.text != '前往雲林布袋戲館' and level == '5')):
         error_Nohelp(event,help)
-    elif((event.message.text == '前往雲林布袋戲館' and level == '5') or event.message.text == 'test6'):
-        level = '6'
-        help == '1'
-        levelsix_message(event)
+    
 
     
     elif((event.message.text == '我需要幫忙' and level == '6')):
@@ -162,11 +213,7 @@ def handle_message(event):
             Help_2(event,level)
     elif((event.message.text != '生、丑' and level == '6')):
         error_Nohelp(event,help)
-    elif((event.message.text == '生、丑' and level == '6') or event.message.text == 'test7'):
-        level = '7'
-        help == '1'
-        levelseven_message(event)
-
+    
 
 
     elif((event.message.text == '我需要幫忙' and level == '7')):
@@ -180,10 +227,7 @@ def handle_message(event):
             Help_3(event,level)
     elif((event.message.text != '屋敷' and level == '7')):
         error_Nohelp(event,help)
-    elif((event.message.text == '屋敷' and level == '7') or event.message.text == 'test8'):
-        level = '8'
-        help == '1'
-        leveleight_message(event)
+    
 
 
     elif((event.message.text == '我需要幫忙' and level == '8')):
@@ -197,10 +241,7 @@ def handle_message(event):
             Help_3(event,level)
     elif(((event.message.text != 'cool' and event.message.text != 'cool') and level == '8')):
         error_Nohelp(event,help)
-    elif(((event.message.text == 'COOL' or event.message.text == 'cool') and level == '8') or event.message.text == 'test9'):
-        level = '9'
-        help == '1'
-        levelnine_message(event)
+    
 
 
     elif((event.message.text == '我需要幫忙' and level == '9')):
@@ -211,10 +252,7 @@ def handle_message(event):
             Help_2(event,level)
     elif((event.message.text != '1921' and level == '9')):
         error_Nohelp(event,help)
-    elif((event.message.text == '1921' and level == '9') or event.message.text == 'test10'):
-        level = '10'
-        help == '1'
-        levelten_message(event)
+    
 
 
     elif((event.message.text == '我需要幫忙' and level == '10')):
@@ -228,10 +266,7 @@ def handle_message(event):
             Help_3(event,level)
     elif((event.message.text != '前往虎珍堂' and level == '10')):
         error_Nohelp(event,help)
-    elif((event.message.text == '前往虎珍堂' and level == '10') or event.message.text == 'test11'):
-        level = '11'
-        help == '1'
-        leveleleven_message(user_id)
+    
     
 
     elif((event.message.text == '我需要幫忙' and level == '11')):
@@ -242,10 +277,7 @@ def handle_message(event):
             Help_2(event,level)
     elif(((event.message.text != 'EADCB' and event.message.text != 'eadcb') and level == '11')):
         error_Nohelp(event,help)
-    elif(((event.message.text == 'EADCB' or event.message.text == 'eadcb') and level == '11') or event.message.text == 'test12'):
-        level = '12'
-        help == '1'
-        leveltwelve_message(event)
+    
 
     
     elif((event.message.text == '我需要幫忙' and level == '12')):
@@ -259,10 +291,7 @@ def handle_message(event):
             Help_3(event,level)
     elif(((event.message.text != '我搭14:00的區間車出發' and event.message.text != '我搭14：00的區間車出發') and level == '12')):
         error_Nohelp(event,help)
-    elif(((event.message.text == '我搭14:00的區間車出發' or event.message.text == '我搭14：00的區間車出發') and level == '12') or event.message.text == 'test13'):
-        level = '13-1'
-        help == '1'
-        levelthirteen_one_message(event)
+    
 
 
     elif((event.message.text == '我需要幫忙' and level == '13-1')):
@@ -273,20 +302,14 @@ def handle_message(event):
             Help_2(event,level)
     elif(((event.message.text != '8' and event.message.text != '八') and level == '13-1')):
         error_Nohelp(event,help)
-    elif(((event.message.text == '8' or event.message.text == '八') and level == '13-1')):
-        level = '13-2'
-        help == '1'
-        levelthirteen_two_message(event)
+    
 
 
     elif((event.message.text == '我需要幫忙' and level == '13-2')):
         Help_1(event,level)
     elif(((event.message.text != '3' and event.message.text != '三') and level == '13-2')):
         error_Nohelp(event,help)
-    elif(((event.message.text == '3' or event.message.text != '三') and level == '13-2')):
-        level = '13-3'
-        help == '1'
-        levelthirteen_three_message(event)
+    
 
 
     elif((event.message.text == '我需要幫忙' and level == '13-3')):
@@ -297,9 +320,7 @@ def handle_message(event):
             Help_2(event,level)
     elif((event.message.text != '五分車' and level == '13-3')):
         error_Nohelp(event,help)
-    elif((event.message.text == '五分車' and level == '13-3') or event.message.text == 'end'):
-        level = 'end'
-        levelend_message(event)
+    
     
 
 

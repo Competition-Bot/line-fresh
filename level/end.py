c10dc0be58c1c4bf6106ae0320758ee8290acb7a
may <may1092200258@gmail.com>
2021-12-03 19:06:03 +0800

    fix - end message

--- level/end.py
+++ level/end.py
@@ -39,6 +39,6 @@ def levelend_message(event):
             TextSendMessage(
                 text="小花當時的冒險就到這邊結束了，回顧著他們倆的互動，我好像也回到了當時的虎尾！不知不覺，就進入了歷史文化的脈絡中。不知道她們倆現在過得怎樣，但我想她們也正在編織她們的人生冒險之旅吧！"),
             TextSendMessage(
-                text="系統回應：您完成了『虎尾手札 - 穿梭巷弄的少女』所有內容，恭喜您獲得遊戲優惠專屬碼oeifejffe，加入『虎尾大儒俠-好康攏底加』社群即可享有優惠！\n加入社群(hand pointing right)https://line.me/ti/g2/G63Po3cBuCcYR4thq0cVh-CXyCfVIRWgmV9UWw?utm_source=invitation&utm_medium=link_copy&utm_campaign=default")
+                text="系統回應：您完成了『虎尾手札 - 穿梭巷弄的少女』所有內容，恭喜您獲得遊戲優惠專屬碼oeifejffe，加入『虎尾大儒俠-好康攏底加』社群即可享有優惠！\n\n加入社群：https://line.me/ti/g2/G63Po3cBuCcYR4thq0cVh-CXyCfVIRWgmV9UWw?utm_source=invitation&utm_medium=link_copy&utm_campaign=default")
         ]
     )
